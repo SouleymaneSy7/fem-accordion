cc0fe1c15ee5367d46e4bed96a6bc1dab070dd36
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-17 01:34:05 +0200

    config: add the constant files for data

---
+++ src/constants/index.ts
@@ -0,0 +1,33 @@
+import starIcon from "@/assets/images/icon-star.svg";
+import backgroundPatternMobile from "@/assets/images/background-pattern-mobile.svg";
+import backgroundPatternDesktop from "@/assets/images/background-pattern-desktop.svg";
+
+export const HeadingTitle = "FAQs";
+
+export const accordionData = [
+  {
+    id: 0,
+    accordionTitle: "What is Frontend Mentor, and how will it help me?",
+    accordionContents:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+  },
+  {
+    id: 1,
+    accordionTitle: "Is Frontend Mentor free?",
+    accordionContents:
+      "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+  },
+  {
+    id: 2,
+    accordionTitle: "Can I use Frontend Mentor projects in my portfolio?",
+    accordionContents:
+      "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+  },
+  {
+    id: 3,
+    accordionTitle:
+      "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    accordionContents:
+      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+  },
+];
