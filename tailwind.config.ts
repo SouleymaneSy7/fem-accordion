ee3910d42705da814535b94a861c23fc180ed2f5
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-17 21:56:22 +0200

    config: add the colors and the font-weight for the tailwindcss config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,16 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "white-clr": "var(--white-clr)",
+      "light-pink-clr": "var(--light-pink-clr)",
+      "grayish-purple-clr": "var(--grayish-purple-clr)",
+      "dark-purple-clr": "var(--dark-purple-clr)",
+    },
+    fontWeight: {
+      "fw-semi-bold": "var(--fw-semi-bold)",
+      "fw-bold": "var(--fw-bold)",
+    },
     extend: {},
   },
   plugins: [],
