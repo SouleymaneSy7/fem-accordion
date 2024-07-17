dc9bd21cee16f95c43fb9ec2c6f5744a879110b1
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-17 02:08:56 +0200

    feat: add the html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-// import React from "react";
+import React from "react";
+import { accordionData, HeadingTitle } from "./constants";
+import Accordion from "./components/Accordion";
+import starIcon from "@/assets/images/icon-star.svg";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <React.Fragment>
+      <main>
+        <h1>
+          <span>
+            <img src={starIcon} alt="An star svg icon" />
+          </span>
+          {HeadingTitle}
+        </h1>
+
+        <section>
+          {accordionData.map(({ id, accordionTitle, accordionContents }) => {
+            return (
+              <Accordion key={id} title={accordionTitle}>
+                {accordionContents}
+              </Accordion>
+            );
+          })}
+        </section>
+      </main>
+    </React.Fragment>
+  );
 };
 
 export default App;
