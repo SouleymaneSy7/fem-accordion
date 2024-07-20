4e9a6b77f8fc0def49397903519b55e6235b71ff
Souleymane Sy <souleymanesyservices@gmail.com>
2024-07-20 20:08:20 +0200

    feat: add the html structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { accordionData, HeadingTitle } from "./constants";
 import Accordion from "./components/Accordion";
 import starIcon from "@/assets/images/icon-star.svg";
+import Attribution from "./components/Attribution";
 
 const App = () => {
   return (
     <React.Fragment>
-      <main>
-        <h1>
+      <main className="bg-white-clr rounded-xl px-[var(--main-padding-x)] py-[var(--main-padding-y)] my-8">
+        <h1 className="flex items-center gap-4 text-fs-heading font-fw-bold text-dark-purple-clr mb-[var(--heading-margin-bottom)]">
           <span>
             <img src={starIcon} alt="An star svg icon" />
           </span>
@@ -24,6 +25,8 @@ const App = () => {
           })}
         </section>
       </main>
+
+      <Attribution />
     </React.Fragment>
   );
 };
